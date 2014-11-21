56ad206d0da1923cc82d24546092a46cb9935265
Lawrence Ma <lawrencema@lawrences-air.att.net>
2014-11-20 21:01:17 -0800

    adds sign-in modal and styles homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'homepage#index'
   resources :users do
-    resources :achievements
+    resources :achievements, except: [:index]
   end
   resources :projects
+  resources :sessions, only: [:create, :destroy]
+  match '/signout', to: 'sessions#destroy', via: 'delete'
+  match '/signin', to: 'sessions#create', via: 'post'
+  get '/logout' => 'sessions#destroy'
 end

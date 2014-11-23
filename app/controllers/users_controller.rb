bb10aa254590315f94b4ba3cf4604658476df19a
Lawrence Ma <lawrencema@Lawrences-MacBook-Air.local>
2014-11-22 21:26:46 -0800

    readies app for heroku push

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,24 @@ class UsersController < ActiveController
   def show
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :about, :rating, :current_project, :email, :password)
+  end
+
 end
